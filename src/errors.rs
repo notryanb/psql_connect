b38b08dedd4e3a1d9fbd16d80d8fc6949f9169c2
Ryan B <notryanb@gmail.com>
2018-06-14 15:22:33 -0400

    remove dead code

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,4 @@
 error_chain! { 
-    // types {
-    //     Error, ErrorKind, ResultExt, Result;
-    // }
     foreign_links {
         Fmt(::std::fmt::Error);
         Io(::std::io::Error) #[cfg(unix)];
